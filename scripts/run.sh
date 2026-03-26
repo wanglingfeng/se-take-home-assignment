bc0d28c40d92896ab6797112f213e6a8a2a17fe1
wanglingfeng <joseph0wlf@gmail.com>
2026-03-26 20:11:46 +0800

    modify

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,7 +6,7 @@
 echo "Running CLI application..."
 
 # For Go projects:
-./order-controller > result.txt
+./order-controller | tee scripts/result.txt
 
 # For Node.js projects:
 # node index.js > result.txt
